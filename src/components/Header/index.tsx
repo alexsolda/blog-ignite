c085b865c6367c70034edf7abed7a3964601931a
Alex Solda <alexsoldaa@gmail.com>
2021-07-30 22:27:17 -0300

    Add: Link to homepage on Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,13 @@
 import styles from './header.module.scss';
+import Link from 'next/link';
 
 export default function Header() {
   return (
     <header>
       <div className={styles.container}>
-        <img src='/assets/logo.svg' alt='logo' />
+        <Link href='/'>
+          <img src='/assets/logo.svg' alt='logo' />
+        </Link>
       </div>
     </header>
   )
